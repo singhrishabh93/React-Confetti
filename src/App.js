6b9b43868bb29ba067ba783880efc40f874d523a
Rishabh Singh <singhrishabh1670@gmail.com>
2023-03-19 00:46:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Confetti from "react-confetti";
+
+function App() {
+  return (
+    <div>
+      <Confetti />
+      Rishabh Singh <br />
+      This is a Confetti
+      
+    </div>
+  );
+}
+
+export default App;
